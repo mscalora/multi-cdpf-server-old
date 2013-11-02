f03e448c8566bb8f3a8d107f896bb6751245c43d
Mike Scalora <mike@scalora.org>
2013-11-02 06:07:11 -0600

    Initial checkin

---
+++ index.php
@@ -0,0 +1,155 @@
+<?php
+	//$dataDir = isset($_ENV['OPENSHIFT_DATA_DIR']) ? $_ENV['OPENSHIFT_DATA_DIR'] : (isset($_SERVER['OPENSHIFT_DATA_DIR']) ? $_SERVER['OPENSHIFT_DATA_DIR'] : ($_ENV["DOCUMENT_ROOT"]."/data/"));
+	$dataDir = "${_SERVER['DOCUMENT_ROOT']}/data/" ;
+	
+	$fromPi = (isset($_REQUEST['list']) && count($_REQUEST)==1)
+		|| (isset($_SERVER["HTTP_USER_AGENT"]) && $_SERVER["HTTP_USER_AGENT"]=="CDPF")
+		|| isset($_REQUEST["CDPF"]);
+	$title = "Connected Digital Photo Frame";
+    $list = isset($_REQUEST['list']) ? $_REQUEST['list'] : '1';
+
+    $count = 1;
+    $selected = 1;
+
+    while($count<100 && is_dir($dataDir.($count+1))) {
+        $count++;
+    }
+
+    if (isset($_REQUEST["count"])) {
+        echo $count;
+        exit;
+    }
+
+    file_put_contents($dataDir.'count.txt',''.$count);
+
+	require_once 'twig/lib/Twig/Autoloader.php';
+	Twig_Autoloader::register();
+	$twig = new Twig_Environment(new Twig_Loader_Filesystem('.'));
+	
+	if (!$fromPi) {
+		require("auth.php");
+	}
+	
+	// test for image file names
+	function isImageName($name) {
+        global $dataDir;
+        // abs path case
+        if (strpos($name,$dataDir)===0) {
+            return preg_match('/^[0-9]+\/[^.\/][^\/]*\.(jpe?g|png|gif)$/i',substr($name,strlen($dataDir)))>0;
+        }
+        // filename only
+        return preg_match('/^[^.\/][^\/]*\.(jpe?g|png|gif)$/i',$name)>0;
+	}
+
+	// for debugging
+	if (isset($_REQUEST['phpinfo'])) {
+		phpinfo();
+		exit;
+	}
+	
+	// handle serving image files 
+	if (isset($_REQUEST['f'])) {
+
+        $file = $dataDir . $_REQUEST['f'];
+
+        if (isImageName($file) && is_file($file)) {
+
+			// set content type if a legal image extention
+			if (preg_match('/\.gif/i',$file)) {
+				header('Content-Type: image/gif');
+			} else if (preg_match('/\.jpe?g/i',$file)) {
+				header('Content-Type: image/jpeg');
+			} else if (preg_match('/\.png/i',$file)) {
+				header('Content-Type: image/png');
+			} else {
+				header ("HTTP/1.0 404 Not Found");
+				exit;
+			}
+
+			// Checking if the client is validating his cache and if it is current.
+			if (isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) && (strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE']) == filemtime($file))) {
+				// Client's cache IS current, so we just respond '304 Not Modified'.
+				header('Last-Modified: '.gmdate('D, d M Y H:i:s', filemtime($file)).' GMT', true, 304);
+				exit;
+			}
+			
+			// insert Last-Modified header for wget's -N option
+			header('Last-Modified: '.gmdate('D, d M Y H:i:s', filemtime($file)).' GMT', true, 200);
+			header('Content-Length: '.filesize($file));
+			// for HEAD requests (which WGET claims to use) we don't send the content
+			if ($_SERVER['REQUEST_METHOD']=='HEAD') {
+				exit;
+			}
+			
+			// tell clients to cache for 5 years
+			$seconds_to_cache = 60*60*24*365*5;
+			$ts = gmdate("D, d M Y H:i:s", time() + $seconds_to_cache) . " GMT";
+			header("Expires: $ts");
+			header("Pragma: cache");
+			header("Cache-Control: max-age=$seconds_to_cache");
+			header('Pragma: public');
+			
+			// send image to client
+			readfile($file);
+			exit;
+			
+		}
+		
+		// error
+		header ("HTTP/1.0 404 Not Found");
+		exit;
+	}
+	
+	$message = "";
+	
+	if (isset($_REQUEST['submit']) && isset($_FILES["the-file"])) {
+	
+		$file = $_FILES["the-file"];
+		$message = "Missing or illegal file name!";
+		
+		if (isset($file["name"]) && isImageName($file["name"])) {
+		
+			$name = preg_replace('/^[.]|[^\w.]/i','-', $file['name']);
+
+            $album = isset($_REQUEST['the-album']) ? $_REQUEST['the-album'] : '1';
+
+            if (preg_match('/^[0-9]+$/',$album)>0) {
+                if (!is_dir($dataDir.$album)) {
+                    mkdir($dataDir.$album);
+                }
+                $result = move_uploaded_file($file["tmp_name"],$dataDir.$album .'/'.$name);
+                $message = $result ? "The photo $name has been uploaded" :
+                    "Error receiving the file: $name";
+            }
+
+		}
+		header('Location: /');
+		exit;
+	}
+
+	$images = array();
+    foreach(range(1,$count) as $i) {
+        $images[$i] = @array_filter(@scandir($dataDir.$i.'/'),'isImageName');
+    }
+
+
+	if (isset($_REQUEST['delete-image'])) {
+		$fileName = $_REQUEST['delete-image'];
+		if (in_array($fileName,$images,true)) {
+			unlink($dataDir.$fileName);
+			header('Location: /');
+			exit;
+		}
+	}
+	
+	echo $twig->render($fromPi ? 'list.twig' : 'index.twig', array(
+		'title' => $title,
+		'message' => $message,
+		'images' => $images,
+		'ui' => !$fromPi,
+        'albumCount' => $count,
+        'albumSelected' => $selected,
+        'list' => $list
+	));
+
+?>
---
+++ thumb.php
@@ -0,0 +1,206 @@
+<?php
+	/**
+	 * Image thumbnail generator
+	 * Based on the work from http://sniptools.com/vault/generating-jpggifpng-thumbnails-in-php-using-imagegif-imagejpeg-imagepng
+     * Based on the work from http://ria-coder.com/blog/php-thumbnail-generator
+	 */
+	class ThumbnailGenerator
+	{
+		public $sourceFile;
+		public $destinationFile;
+		public $width;
+		public $height;
+		public $format;
+		public $scale;
+ 
+		/**
+		 * ThumbnailGenerator::__construct()
+		 *
+		 * @param mixed $sourceFile Path to source file
+		 * @param mixed $destinationFile Path to destination file
+		 * @param mixed $width Thumbnail file width
+		 * @param mixed $height Thumbnail file height
+		 * @param string $format jpeg, png or gif
+		 * @param bool $scale Scale thumbnail
+		 * @return
+		 */
+		public function __construct($sourceFile, $destinationFile, $width, $height, $format="png", $quality=9, $transparent=true, $useResample=true) {
+			$this->sourceFile = $sourceFile;
+			$this->destinationFile = $destinationFile;
+			$this->width = $width;
+			$this->height = $height;
+            $this->format = $format;
+            $this->outputFormat = $format;
+            $this->quality = $quality;
+            $this->transparent = $transparent;
+            $this->useResample = $useResample;
+        }
+
+        /**
+         * @param bool $crop - true=scale one dimention and crop the other
+         * @return bool
+         */
+        public function _generate2($crop=false) {
+            $fromFormat = "imagecreatefrom".$this->format;
+            $sourceImage = $fromFormat($this->sourceFile);
+            $sourceWidth = imagesx($sourceImage);
+            $sourceHeight = imagesy($sourceImage);
+
+            if ($this->width===0 || $this->height===0 || $sourceWidth===0 || $sourceHeight===0) return false;
+
+            $wRatio = $sourceWidth/$this->width;
+            $hRatio = $sourceHeight/$this->height;
+
+            if ($crop ? $wRatio<$hRatio : $wRatio>$hRatio) {
+                $destWidth = $this->width;
+                $destHeight = $sourceHeight/$wRatio;
+                $destY = ($this->height-$destHeight)/2;
+                $destX = 0;
+            } else {
+                $destHeight = $this->height;
+                $destWidth = $sourceWidth/$hRatio;
+                $destX = ($this->width-$destWidth)/2;
+                $destY = 0;
+            }
+
+
+            $targetImage = imagecreatetruecolor($this->width,$this->height);
+            // preserve transparency
+            if($this->transparent && ($this->format == "gif" or $this->format == "png")){
+                imagealphablending($targetImage, false);
+                $colorTransparent = imagecolorallocatealpha($targetImage, 0, 0, 0, 127);
+                imagefill($targetImage, 0, 0, $colorTransparent);
+                imagesavealpha($targetImage, true);
+            }
+
+            $copyFunction = $this->useResample ? "imagecopyresampled" : "imagecopyresized";
+            $copyFunction($targetImage,$sourceImage,$destX,$destY,0,0,$destWidth,
+                    $destHeight,imagesx($sourceImage),imagesy($sourceImage));
+
+            $imageFunction = "image".$this->outputFormat;
+            $return = $this->format=='gif' ?
+                $imageFunction($targetImage, $this->destinationFile) :
+                $imageFunction($targetImage, $this->destinationFile, $this->getQuality());
+            imagedestroy($targetImage);
+            return $return;
+        }
+
+        public function setOutputFormat($format) { $this->outputFormat = $format; }
+        public function setQuality($quality) { $this->quality = $quality; }
+        public function setTransparent($transparent) { $this->transparent = $transparent; }
+        public function setUseResample($useResample) { $this->useResample = $useResample; }
+
+        public function getQuality() {
+            $quality = $this->quality;
+            if ($this->outputFormat==='png') {
+                return $quality<0 ? 0 : $quality>9 ? 9 : $quality;
+            } else {
+                return $quality<0 ? 0 : $quality>100 ? 100 : $quality;
+            }
+        }
+
+        /**
+         * @param null $hangOver - true=hang over,  false=fit in,  null=scale down to fit within both
+         * @return bool
+         */
+        public function _generate($hangOver=null) {
+			$fromFormat = "imagecreatefrom".$this->format;
+			$sourceImage = $fromFormat($this->sourceFile);
+			$sourceWidth = imagesx($sourceImage);
+			$sourceHeight = imagesy($sourceImage);
+
+            if ($this->width===0 || $this->height===0 || $sourceWidth===0 || $sourceHeight===0) return false;
+
+            $wRatio = $sourceWidth/$this->width;
+            $hRatio = $sourceHeight/$this->height;
+
+            if($hangOver!==null) {
+                $ratio = ($hangOver ? $wRatio<$hRatio : $wRatio>$hRatio) ? $wRatio : $hRatio;
+				$this->width = $sourceWidth / $ratio;
+				$this->height = $sourceHeight / $ratio;
+			}
+
+            $targetImage = imagecreatetruecolor($this->width,$this->height);
+            // preserve transparency
+            if($this->transparent && ($this->format == "gif" or $this->format == "png")){
+                imagealphablending($targetImage, false);
+                $colorTransparent = imagecolorallocatealpha($targetImage, 0, 0, 0, 127);
+                imagefill($targetImage, 0, 0, $colorTransparent);
+                imagesavealpha($targetImage, true);
+            }
+
+            $copyFunction = $this->useResample ? "imagecopyresampled" : "imagecopyresized";
+            $copyFunction($targetImage,$sourceImage,0,0,0,0,$this->width,
+                $this->height,imagesx($sourceImage),imagesy($sourceImage));
+
+			$imageFunction = "image".$this->outputFormat;
+			$return = $this->format=='gif' ?
+				$imageFunction($targetImage, $this->destinationFile) :
+				$imageFunction($targetImage, $this->destinationFile, $this->getQuality());
+			imagedestroy($targetImage);
+			return $return;
+		}
+
+        /**
+         *  squish to fit in both dimensions exactly
+         */
+        public function squish() {
+            $this->_generate();
+        }
+
+        /**
+         *   proportionally scale to fit within both dimensions
+         */
+        public function fitIn() {
+            $this->_generate(false);
+        }
+
+        /**
+         *   size to fit in one target dimensions and then hang over in the other
+         */
+        public function hangOver() {
+            $this->_generate(true);
+        }
+
+        /**
+         *   letter-box to the target dimensions
+         */
+        public function letterBox() {
+            $this->_generate2(false);
+        }
+
+        /**
+         *   scale to fit in one dimensions and crop to fit the other
+         */
+        public function crop() {
+            $this->_generate2(true);
+        }
+    }
+
+
+	//========================================
+    
+    $name = isset($_REQUEST['name']) ? basename($_REQUEST['name']) : false;
+    $thumb = "thumb/$name";
+    $file = "sync/$name";
+    
+    if ($name===false || !is_file($file) || !preg_match('/^[^\/]+\.(jpg|jpeg|png|gif)$/i',$name,$r) || preg_match('/\//i',$name)) {
+	    header('Content-type: image/png');
+    	readfile('error.png');
+    	exit;
+    } 
+    
+    $type = strtolower($r[1])==='jpg' ? 'jpeg' : strtolower($r[1]);
+    header('Content-type: image/'.$type);
+    
+    if (is_file($thumb) && filemtime($thumb)>filemtime($file)) {
+    	readfile('thumb/' . $name);
+    	exit;		    	
+	}
+    
+	$tng = new ThumbnailGenerator($file,$thumb,1440/4,900/4,$type,60);
+	$result = $tng->letterBox();
+	
+   	readfile('thumb/' . $name);
+    
+?>
